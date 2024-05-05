70fb070556f923a35cf855ec914302763e9ea88c
hhabichuela <165214212+hhabichuela@users.noreply.github.com>
2024-05-05 01:02:34 -0300

    ahora si esto entrego
    5.5

--- albumes.js
+++ albumes.js
@@ -1,20 +1,20 @@
 let albFavoritos = [];
 let spansAlbumes = document.querySelectorAll('.star');
 
-function favoritos(span) {
-    let album = span.getAttribute('data-album');
-    // intenté hacerlo con for pero me causaba problemas para sacar y volver a poner el fav 
-    if (!albFavoritos.includes(album)) {
-
-        albFavoritos.push(album);
-        span.textContent = "★";
-        alert(album + " fue agregado a favoritos.");
-    } else {
-
-        let index = albFavoritos.indexOf(album);
-        albFavoritos.splice(index, 1);
-        span.textContent = "☆";
-        alert(album + " fue sacado de favoritos.");
-    } 
-
-}
+spansAlbumes.forEach(span => {
+    span.addEventListener('click', function() {
+        let album = this.getAttribute('data-album');
+        if (!albFavoritos.includes(album)) {
+            albFavoritos.push(album);
+            this.querySelector('i').classList.remove('fa-regular');
+            this.querySelector('i').classList.add('fa-solid');
+            alert(album + " fue agregado a favoritos.");
+        } else {
+            let index = albFavoritos.indexOf(album);
+            albFavoritos.splice(index, 1);
+            this.querySelector('i').classList.remove('fa-solid');
+            this.querySelector('i').classList.add('fa-regular');
+            alert(album + " fue sacado de favoritos.");
+        }
+    });
+});
